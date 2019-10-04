8d11e09d21eacf065cc3df74c9d931bb3818a238
fjhussai <farwajhussain@gmail.com>
2019-10-04 16:07:52 -0500

    add do you like pizza

---
+++ go-play/pizza.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Do you like Pizza? \n")
+	var likespizza string
+	fmt.Scanln(&likespizza)
+
+	if (likespizza) == "no" {
+		fmt.Println("Thank you for your response")
+	} else {
+		fmt.Println("What kind of Piza do you like?")
+	}
+
+}
